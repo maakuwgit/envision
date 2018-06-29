cd3a87ffe3f92ed802440ca3be31b71783254de2
MarkW <mark@liftedlogic.com>
2018-06-29 07:27:42 -0500

    Contact form work

--- templates/contents/content-form.php
+++ templates/contents/content-form.php
@@ -1,15 +1,14 @@
 <?php
   $form_id = ( get_field('form_id') ? get_field('form_id') : 1 );
+  $superheader = get_field('superheader');
 ?>
 <article <?php post_class('form-skin'); ?>>
   <header class="gform_heading">
-    <h1 class="grey">The Best Medical Spa in Rochester New York</h1>
-    <h2 class="h0 gform_title"><?php the_title(); ?></h2>
-    <?php the_excerpt(); ?>
+    <h1 class="gform_supertitle"><?php echo $superheader; ?></h1>
   </header>
   <?php if( is_plugin_active( 'gravityforms/gravityforms.php' ) ) : ?>
 
-    <?php gravity_form( $form_id, false, false ); ?>
+    <?php gravity_form( $form_id, true, true ); ?>
 
   <?php endif; ?>
 
